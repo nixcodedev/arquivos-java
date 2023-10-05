c98420470ae04b4d3acddee0c1989299789ad2d5
Nixcodedev <nixon.codedev@gmail.com>
2023-10-04 22:02:45 -0300

    Erro em: Criando Planilha Excel XLS com Apache POI

--- arquivos-java/src/arquivos/ApachePoi.java
+++ arquivos-java/src/arquivos/ApachePoi.java
@@ -14,7 +14,8 @@ public class ApachePoi {
 
 	public static void main(String[] args) throws Exception {
 
-		File file = new File("/home/nino/git/arquivos-java/arquivos-java/src/arquivos/aquivo_excel.slx");
+		File file = new File
+				("/home/nino/git/arquivos-java/arquivos-java/src/arquivos/aquivo_excel.xls");
 
 		if (!file.exists()) {
 			file.createNewFile();
@@ -38,37 +39,38 @@ public class ApachePoi {
 
 		/* Pode vir do banco de dados ou quelquer outra fonte de dados */
 		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
 		pessoas.add(pessoa1);
 		pessoas.add(pessoa2);
 		pessoas.add(pessoa3);
 
 		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(); /* VAI SER USADO PARA ESCREVER A PLANILHA */
-		HSSFSheet linhasPessoa = hssfWorkbook
-				.createSheet("Planilha de pessoas JDev Treinamento"); /* Criar a planilha */
-		
+		HSSFSheet linhasPessoa =
+				hssfWorkbook.createSheet("Planilha de pessoas JDev Treinamento"); /* Criar a planilha */
+
 		int numeroLinha = 0;
 		for (Pessoa p : pessoas) {
-			Row linha = linhasPessoa.createRow(numeroLinha ++); /* Criando a linha na planilha */
-			
+			Row linha = linhasPessoa.createRow(numeroLinha++); /* Criando a linha na planilha */
+
 			int celula = 0;
-			
-			Cell celNome = linha.createCell(celula ++); //Celula 1
+
+			Cell celNome = linha.createCell(celula++); // Celula 1
 			celNome.setCellValue(p.getNome());
-			
-			Cell celEmail = linha.createCell(celula ++); //Celula 2
+
+			Cell celEmail = linha.createCell(celula++); // Celula 2
 			celEmail.setCellValue(p.getEmail());
-			
-			Cell celIdade = linha.createCell(celula ++); //Celula 1
+
+			Cell celIdade = linha.createCell(celula++); // Celula 1
 			celIdade.setCellValue(p.getIdade());
-		}//Terminou de montar a planilha
-		
+		} // Terminou de montar a planilha
+
 		FileOutputStream saida = new FileOutputStream(file);
-		hssfWorkbook.write(saida); //Escreve planilha em arquivo
+		hssfWorkbook.write(saida); // Escreve planilha em arquivo
 		saida.flush();
 		saida.close();
-		
+
 		System.out.println("Planilha foi criada");
-		
+
 	}
 
 }
